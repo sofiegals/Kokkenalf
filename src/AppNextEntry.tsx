b7526df413447ac81f0f00cba41a471bacb77607
sofiegals <sgalskov@hotmail.com>
2026-09-08 10:00:52 +0200

    Tilføj råvarefinder som tydelig funktion

--- src/AppNextEntry.tsx
+++ src/AppNextEntry.tsx
@@ -1,2 +1,5 @@
+import React,{useState}from'react';
 import AppNext from './AppNext';
-export default AppNext;
+import PantryMatch from './PantryMatch';
+import './PantryMatch.css';
+export default function AppNextEntry(){const[open,setOpen]=useState(false);return <><AppNext/><button className="ka-pantry-launch" onClick={()=>setOpen(true)}>🥕 Hvad har vi hjemme?</button>{open&&<div className="ka-pantry-overlay"><PantryMatch onBack={()=>setOpen(false)}/></div>}</>}
